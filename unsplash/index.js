d48b8c6910f63821aab4e8df417755b1dd1f5d69
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-05-31 14:54:43 +0800

    Add unsplash intergration

--- unsplash/index.js
+++ unsplash/index.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { config } from 'dotenv';
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url); 
 const __dirname = dirname(__filename);
 
 config({ path: `${__dirname}/../.env` });
