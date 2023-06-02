b3341bf8dd1961f879fdf4b134a135cde39bf712
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-06-02 22:27:55 +0800

    Add error handling

--- unsplash/keywordFinder.js
+++ unsplash/keywordFinder.js
@@ -12,26 +12,38 @@ export async function findKeywords(script) {
 
   // Iterate over the sentences and find a keyword for each one
   for (let i = 0; i < sentences.length; i++) {
-    try {
-      const importantKeyword = await createCompletion(`
-        I will give you a sentence. Choose a keyword to represent that sentence.
-        The keyword will be used to search for a picture on Unsplash.
-        Just write the name of the keyword.  Make it so that the keyword should be a term that can return results from unsplash.
-        Here is the sentence:
-        ${sentences[i]}`);
+    let retries = 3;  // Number of times to retry the operation
 
-      let keyword = importantKeyword.choices[0].text.trim();
-      console.log("Keyword : ", keyword)
+    for (let j = 0; j < retries; j++) {
+      try {
+        const importantKeyword = await createCompletion(`
+          I will give you a sentence. Choose a keyword to represent that sentence.
+          The keyword will be used to search for a picture on Unsplash.
+          Just write the name of the keyword.  Make it so that the keyword should be a term that can return results from unsplash.
+          Here is the sentence:
+          ${sentences[i]}`);
 
-      // push the keyword into our array
-      keywordArray.push(keyword);
+        let keyword = importantKeyword.choices[0].text.trim();
+        console.log("Keyword : ", keyword);
 
-    } catch (error) {
-      console.error(error)
+        // push the keyword into our array
+        keywordArray.push(keyword);
+
+        // If operation was successful, break out of the retry loop
+        break;
+      } catch (error) {
+        console.error(`Error on attempt ${j + 1}: `, error);
+
+        // If this was the last attempt, throw the error
+        if (j === retries - 1) {
+          throw error;
+        }
+
+        // Wait for a bit before retrying
+        await new Promise(resolve => setTimeout(resolve, 1000 * (j + 1)));
+      }
     }
   }
 
   return keywordArray;
 }
-
-
